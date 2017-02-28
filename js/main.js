0ec5470f91fc0d23f5cab74222678c4b857d3056
pathakashay <pathakashay@yahoo.com>
2017-03-01 00:14:34 +0530

    This is main Vue file
    This file contains all the Vue components as well as data

---
+++ js/main.js
@@ -0,0 +1,82 @@
+// this component here is used for on click editing of the note with the use of template.
+var NoteComponent = Vue.extend
+({
+  data: function()
+  {
+    return{inEditMode: false}
+  },
+  props: ['note'],
+  template: '<div><span v-on:click="clicked" v-show = "!inEditMode">{{note.text}}</span><input v-on:keyup.enter="saved" v-model="note.text" v-show="inEditMode" required></div>',
+
+  methods:
+  {
+    clicked: function()
+    {
+
+      this.inEditMode = true;
+    },
+    saved: function()
+    {
+      var edited = this.note.text.trim()
+      if(edited)
+      {
+      this.inEditMode = false;
+      }
+    }
+  }
+});
+
+// this vue component contains all the funtion used for on click button. 
+Vue.component('note', NoteComponent);
+
+
+new Vue
+({
+  el: '#app',
+  data:
+    {
+      count: 1,
+      noteText:'',
+      archiveNote:'',
+      notes:
+      [
+        {text: 'Note 1'},
+        {text: 'Note 2'},
+      ],
+
+      archive:
+        [
+          {text:'' },
+
+        ],
+      showNotes: true
+    },
+
+    methods:
+    {
+      createNote: function(e)
+      {
+        e.preventDefault();
+        var note = this.noteText.trim()
+        if(note)
+        {
+          this.notes.push
+          ({
+            text: note
+          });
+          this.noteText = '';
+        }
+      },
+      clearNote: function()
+      {
+        this.noteText = ''
+      },
+
+      removeNote:function(note)
+      {
+        this.notes.splice(this.notes.indexOf(note),1);
+      },
+
+
+    }
+});
